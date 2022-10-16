15d820583268e2847b93602a45ff0e16ecc15ca3
mehran <mehranfo12@gmail.com>
2022-10-16 10:51:19 +0330

    adding if tag to template and modifying context of index view

--- django_4h/meetups/views.py
+++ django_4h/meetups/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index (request) :
+
+
+def index(request):
     meetups = [
-        {"title" : "first meetup"},
-        {"title" : "second meetup"},
+        {
+            "title": "first meetup",
+            "grade": 12,
+        },
+        {
+            "title": "second meetup",
+            "grade": 4,
+        },
     ]
     context = {
-        "meetups" : meetups
+        "meetups": meetups,
+        "show_meetups":  True,
     }
-    return render(request,"meetups/index.html",context=context) 
-
+    return render(request, "meetups/index.html", context=context)
