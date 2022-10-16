22850a4c1bb3c66295b5d015a7d705d037c73fc2
mehran <mehranfo12@gmail.com>
2022-10-16 10:19:32 +0330

    sending context from view.py to template anf how to acsses that

--- django_4h/meetups/views.py
+++ django_4h/meetups/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request) :
+    meetups = [
+        {"title" : "first meetup"},
+        {"title" : "second meetup"},
+    ]
     context = {
-
+        "meetups" : meetups
     }
     return render(request,"meetups/index.html",context=context) 
 
