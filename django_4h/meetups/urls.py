5a4696564bb88afe98a228fd8c0c63e7842e618e
mehran <mehranfo12@gmail.com>
2022-10-17 20:51:06 +0330

    creating detail_page template

--- django_4h/meetups/urls.py
+++ django_4h/meetups/urls.py
@@ -4,4 +4,6 @@ from .import views
 
 urlpatterns = [
     path('index/', views.index , name = 'home-page'),
+    path('index/<slug:meetup_slug>', views.meetup_details , name = 'meetup_details'),
+
 ]
--- django_4h/meetups/views.py
+++ django_4h/meetups/views.py
@@ -1,3 +1,4 @@
+from this import d
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -22,3 +23,17 @@ def index(request):
         "show_meetups":  True,
     }
     return render(request, "meetups/index.html", context=context)
+
+
+def meetup_details(request, meetup_slug):
+
+    meetup = {
+        "title": "first meetup",
+        "address": "Paris",
+        "slug": "a-first-meetup",
+    }
+    context = {
+        'meetup': meetup,
+        'slug': meetup_slug,
+    }
+    return render(request, "meetups/meetup_details.html", context=context)
