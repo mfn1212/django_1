b5d10262682456bab38c870d81865bf560d0bafc
mehran <mehranfo12@gmail.com>
2022-10-24 11:01:21 +0330

    showing title of meetup as its name in admin panel

--- django_4h/meetups/models.py
+++ django_4h/meetups/models.py
@@ -7,4 +7,7 @@ class Meetup(models.Model):
     desciption = models.TextField()
     image = models.ImageField(upload_to = 'images',null = True) 
 
+    def __str__(self):
+        return self.title
+
     
