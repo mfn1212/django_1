71a999d998f6e514c8fd33536e0ec8c786fc8bf5
mehran <mehranfo12@gmail.com>
2022-10-24 13:23:25 +0330

    creating location model and many to one reletionship and creating location and meetup in admin panel

--- django_4h/meetups/admin.py
+++ django_4h/meetups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from  .models import Meetup
+from  .models import Meetup,Location
 # Register your models here.
 class MeetupAdmin (admin.ModelAdmin) :
     list_display = ['title','slug','desciption']
@@ -8,3 +8,4 @@ class MeetupAdmin (admin.ModelAdmin) :
         'slug' : ['title']
     }
 admin.site.register(Meetup,MeetupAdmin)
+admin.site.register(Location)
--- django_4h/meetups/models.py
+++ django_4h/meetups/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Location (models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     desciption = models.TextField()
     image = models.ImageField(upload_to = 'images',null = True) 
+    address = models.ForeignKey(Location ,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
