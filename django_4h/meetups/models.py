46003840b5d058e04000e9a39c12d2e0a816f2ec
mehran <mehranfo12@gmail.com>
2022-10-24 09:38:10 +0330

    creating imagefield for meetup class in meetups/models.py and creating meetup whith image in admin panel

--- django_4h/meetups/models.py
+++ django_4h/meetups/models.py
@@ -5,3 +5,6 @@ class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     desciption = models.TextField()
+    image = models.ImageField(upload_to = 'images',null = True) 
+
+    
