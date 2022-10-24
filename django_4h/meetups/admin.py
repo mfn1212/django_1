69f6de36a8dc2f94740820ba89f8f91e6217d7b6
mehran <mehranfo12@gmail.com>
2022-10-24 11:47:13 +0330

    creating auto populating for slug in admin panel

--- django_4h/meetups/admin.py
+++ django_4h/meetups/admin.py
@@ -4,4 +4,7 @@ from  .models import Meetup
 class MeetupAdmin (admin.ModelAdmin) :
     list_display = ['title','slug','desciption']
     list_filter = ["title"]
+    prepopulated_fields= {
+        'slug' : ['title']
+    }
 admin.site.register(Meetup,MeetupAdmin)
