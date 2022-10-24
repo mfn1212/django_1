ea448da8bfd1085cab631f5cd29c9a0d04a836ef
mehran <mehranfo12@gmail.com>
2022-10-24 16:50:33 +0330

    creating participant model and many to many relentionship

--- django_4h/meetups/models.py
+++ django_4h/meetups/models.py
@@ -2,20 +2,27 @@ from django.db import models
 
 # Create your models here.
 
+
+class Participant (models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+
+
 class Location (models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
+
     def __str__(self):
         return f'{self.name} ({self.address})'
+
 
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     desciption = models.TextField()
-    image = models.ImageField(upload_to = 'images',null = True) 
-    address = models.ForeignKey(Location ,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images', null=True)
+    address = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True)
 
     def __str__(self):
         return self.title
-
-    
